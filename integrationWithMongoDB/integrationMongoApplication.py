428ba9867d34c8ccad2af0e1b490adf1044e2944
kadekaro <kadekaroshop@gmail.com>
2023-08-17 12:00:39 -0300

    first commit

---
+++ integrationWithMongoDB/integrationMongoApplication.py
@@ -0,0 +1,50 @@
+import datetime
+import pprint
+
+import pymongo as pym
+
+client = pym.MongoClient("mongodb+srv://kadekaro:kiyoshi123@clusterteste.zjv7moo.mongodb.net/?retryWrites=true&w"
+                         "=majority")
+db = client.test
+collection = db.test_collection
+print(f"\n{db.test_collection}")
+
+# Definição de informação para definir o documento:
+post = {
+    "autor": "Wesley",
+    "text": "My first MongoDB applicated based on python",
+    "tags": ["mongodb", "python3", "pymongo"],
+    "date": datetime.datetime.utcnow()
+}
+
+# preparando para submeter as informações:
+posts = db.posts
+post_id = posts.insert_one(post).inserted_id
+print(f"\n{post_id}\n")
+
+# print(db.posts.find_one())
+pprint.pprint(db.posts.find_one())
+
+# bulk inserts:
+new_posts = [{
+    "author": "Nelsa",
+    "text": "Another post",
+    "tags": ["bulk", "post", "insert"],
+    "date":datetime.datetime.utcnow()
+    },
+    {
+    "author": "Márcia",
+    "text": "Post from Márcia. New post available",
+    "title": "MongoDB Atlas is Fun",
+    "date": datetime.datetime(2009, 11, 10, 10, 45)
+    }]
+
+result = posts.insert_many(new_posts)
+print(f'\nRecuperando os ids:\n{result.inserted_ids}\n')
+
+print("\nRecuperando informações:")
+pprint.pprint(db.posts.find_one({"author": "Márcia"}))
+
+print("\nRecuperando muitas informações de uma vez:")
+for post in posts.find():
+    pprint.pprint(post)
---
+++ integrationWithMongoDB/secondFilePyMongo.py
@@ -0,0 +1,63 @@
+import pymongo
+import pymongo as pym
+import pprint
+
+from bson import ObjectId
+
+# Conectando ao cluster MongoDB
+client = pym.MongoClient("mongodb+srv://kadekaro:kiyoshi123@clusterteste.zjv7moo.mongodb.net/?retryWrites=true&w"
+                         "=majority")
+
+# Acessando o banco de dados e a coleção
+db = client.test
+posts = db.posts
+
+# Imprimindo todos os documentos da coleção 'posts'
+print("Documentos na coleção 'posts':")
+for post in posts.find():
+    pprint.pprint(post)
+
+# Contando o número total de documentos na coleção 'posts'
+print("\nTotal de documentos na coleção 'posts':", posts.count_documents({}))
+
+# Contando o número de documentos na coleção 'posts' com o autor 'Márcia'
+print("Total de documentos na coleção 'posts' com autor 'Márcia':", posts.count_documents({"author": "Márcia"}))
+
+# Contando o número de documentos na coleção 'posts' com a tag 'bulk'
+print("Total de documentos na coleção 'posts' com tag 'bulk':", posts.count_documents({"tags": "bulk"}))
+
+# Encontrando um documento com a tag 'insert'
+pprint.pprint(posts.find_one({"tags": "insert"}))
+
+# Recuperando informações da coleção 'posts' de maneira ordenada por data
+print("\nRecuperando informações da coleção 'posts' de maneira ordenada por data:")
+for post in posts.find({}).sort("date"):
+    pprint.pprint(post)
+
+# Criando um índice único na coleção 'profiles' para a chave 'author'
+result = db.profiles.create_index([('author', pymongo.ASCENDING)], unique=True)
+print(sorted(list(db.profiles.index_information())))
+
+# Inserindo documentos na coleção 'profile_user'
+user_profile_user = [{'user_id': 211, 'name': 'Luke'},
+                     {'user_id': 212, 'name': 'wesley'}]
+results = db.profile_user.insert_many(user_profile_user)
+
+# Listando as coleções armazenadas no MongoDB
+print("\nColeções armazenadas no MongoDB:")
+collections = db.list_collection_names()
+for collection in collections:
+    print(collection)
+
+# Deletando um documento com o autor 'Wesley' da coleção 'posts'
+posts.delete_one({"author": "Wesley".strip()})
+
+# Supondo que o _id seja ObjectId('64de23fc970adf909fde5e36')
+posts.delete_one({"_id": ObjectId('64de23fc970adf909fde5e36')}) # -> excluiu o usuário Wesley pelo ObjectID
+
+# Imprimindo os documentos restantes na coleção 'posts' após a exclusão
+print("\nDocumentos na coleção 'posts' após a exclusão:")
+for post in posts.find():
+    pprint.pprint(post)
+
+# client.drop_database('test') # Exclui o banco de dados
